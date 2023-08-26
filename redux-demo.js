b638f3d3e1f63b4de5288c1c2245966348466f6a
amogh4319 <amogh4319@gmail.com>
2023-08-26 12:20:05 +0530

    increment decrement by2

--- redux-demo.js
+++ redux-demo.js
@@ -1,13 +1,13 @@
 const redux=require('redux');
 const counterReducer=(state={counter:0},action)=>{
-   if(action.type==='increment'){
+   if(action.type==='incrementBy2'){
         return{
     
-            counter:state.counter+1
+            counter:state.counter+2
         }
-    }else if(action.type==='decrement'){
+    }else if(action.type==='decrementBy2'){
         return{
-        counter:state.counter-1
+        counter:state.counter-2
         }
     }
     return state;
@@ -22,11 +22,15 @@ console.log(counterReducer)
     console.log(latestState);
  };
  store.subscribe(counterSubscriber);
- for(let i=0;i<5;i++){
-    store.dispatch({type:'increment'});
- }
- 
- store.dispatch({type:'decrement'})
+// 
+for(let i=0;i<5;i++){
+    store.dispatch({type:'incrementBy2'});
+}
+for(let i=0;i<5;i++){
+    store.dispatch({type:'decrementBy2'});
+}
+
+
 
 
 
