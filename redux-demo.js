334f92fd27e54ce2adb69bbb74334f98574fa3f2
amogh4319 <amogh4319@gmail.com>
2023-08-26 12:06:15 +0530

    redux increment and decrement

---
+++ redux-demo.js
@@ -0,0 +1,32 @@
+const redux=require('redux');
+const counterReducer=(state={counter:0},action)=>{
+   if(action.type==='increment'){
+        return{
+    
+            counter:state.counter+1
+        }
+    }else if(action.type==='decrement'){
+        return{
+        counter:state.counter-1
+        }
+    }
+    return state;
+   
+   
+};
+console.log(counterReducer)
+ const store=redux.createStore(counterReducer);
+
+ const counterSubscriber=()=>{
+    const latestState=store.getState();
+    console.log(latestState);
+ };
+ store.subscribe(counterSubscriber);
+ for(let i=0;i<5;i++){
+    store.dispatch({type:'increment'});
+ }
+ 
+ store.dispatch({type:'decrement'})
+
+
+
