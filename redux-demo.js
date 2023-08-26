2a6b633171612adaafdbfafba783089a4c24aa18
amogh4319 <amogh4319@gmail.com>
2023-08-26 12:23:19 +0530

    added another way to increment decrement

--- redux-demo.js
+++ redux-demo.js
@@ -1,11 +1,21 @@
 const redux=require('redux');
 const counterReducer=(state={counter:0},action)=>{
-   if(action.type==='incrementBy2'){
+   if(action.type==='increment'){
         return{
     
-            counter:state.counter+2
+            counter:state.counter+1
         }
-    }else if(action.type==='decrementBy2'){
+    }else if(action.type==='decrement'){
+        return{
+        counter:state.counter-1
+        }
+    }
+    else if(action.type==='incrementBy2'){
+        return{
+        counter:state.counter+2
+        }
+    }
+    else if(action.type==='decrementBy2'){
         return{
         counter:state.counter-2
         }
